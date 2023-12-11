1279dd991006d490e19073cbfa35da8f9ee3b1f0
Albert Nasretdinov <alnasretdinoff@yandex.ru>
2023-12-11 13:49:34 +0300

    Updated scripts

--- misc/pdf_to_txt.py
+++ misc/pdf_to_txt.py
@@ -1,15 +1,20 @@
 import os
 import PyPDF2
 
-# Define the input and output folders
-input_folder = r"C:\Users\Asus\Documents\Programming_Projects\my2pdf2csv\pdf_files"
-output_folder = r"C:\Users\Asus\Documents\Programming_Projects\my2pdf2csv\txt_files"
+# Absolute address of the project 
+script_path: str = os.path.abspath(__file__)
 
-# Function to extract text from a PDFvenv\Scripts\Activate.ps1
+# Absolute address of the "txt_files" folder 
+input_folder: str = os.path.abspath(os.path.join(script_path, '..', '..', 'pdf_files'))
+
+# Absolute address of the "csv_files" folder 
+output_folder: str = os.path.abspath(os.path.join(script_path, '..', '..', 'txt_files'))
+
+# Function to extract text from a PDF
 def extract_text_from_pdf(pdf_path):
     with open(pdf_path, 'rb') as pdf_file:
         reader = PyPDF2.PdfReader(pdf_file)
-        text = ''
+        text: str = ''
         for page_num in range(len(reader.pages)):
             text += reader.pages[page_num].extract_text()
         return text
@@ -18,11 +23,11 @@ def extract_text_from_pdf(pdf_path):
 for root, _, files in os.walk(input_folder):
     for file in files:
         if file.endswith('.pdf'):
-            pdf_path = os.path.join(root, file)
+            pdf_path: str = os.path.join(root, file)
             # Extract text from the PDF
-            text = extract_text_from_pdf(pdf_path)
+            text: str = extract_text_from_pdf(pdf_path)
             # Save the extracted text to a TXT file in the output folder
-            txt_filename = file.replace('.pdf', '.txt')
-            txt_path = os.path.join(output_folder, txt_filename)
-            with open(txt_path, 'w', encoding='utf-8') as txt_file:
+            txt_filename: str = file.replace('.pdf', '.txt')
+            txt_path: str = os.path.join(output_folder, txt_filename)
+            with open(txt_path, 'w', encoding = 'utf-8') as txt_file:
                 txt_file.write(text)
--- misc/txt_to_csv.py
+++ misc/txt_to_csv.py
@@ -2,40 +2,41 @@ import os
 import re
 import csv
 
-def extract_data_from_text(text, file_name):
+def extract_data_from_text(text: str, file_name:str):
     # Extraction of Ru phone numbers with different formats
-    phone_numbers = re.findall(r'(\+7|\b8)[\s\(]*(\d{3})[\)]?\s*(\d{3})[-]?\s*(\d{2})[-]?\s*(\d{2})', text) or ['phone']
+    phone_numbers: list[str] = re.findall(r'(\+7|\b8)[\s\(]*(\d{3})[\)]?\s*(\d{3})[-]?\s*(\d{2})[-]?\s*(\d{2})', text) or ['phone']
 
     # Convert found phone numbers to the Ru format +7 (XXX) XXX-XX-XX-XX
-    formatted_phone_numbers = [
+    
+    formatted_phone_numbers: list[str] = [
         f"+7 ({match[1]}) {match[2]}-{match[3]}-{match[4]}" if match[0] == '+7' else f"8 ({match[1]}) {match[2]}-{match[3]}-{match[4]}"
         for match in phone_numbers
     ]
 
     # Extracting names in the Ru format 
-    names = re.findall(r'\b[А-Я][а-я]+\s[А-Я][а-я]+(?:\s[А-Я][а-я]+)?', text) or ['name']
+    names: list[str] = re.findall(r'\b[А-Я][а-я]+\s[А-Я][а-я]+(?:\s[А-Я][а-я]+)?', text) or ['name']
 
     # Extracting e-mails 
-    emails = re.findall(r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b', text) or ['email']
+    emails: list[str] = re.findall(r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b', text) or ['email']
 
     # Getting a file name without extension 
-    file_name_without_extension = os.path.splitext(file_name)[0]
+    file_name_without_extension: str = os.path.splitext(file_name)[0]
 
     return formatted_phone_numbers, names, emails, file_name_without_extension
 
-def process_files(input_folder, output_folder):
-    csv_file_path = os.path.join(output_folder, 'extracted_data.csv')
+def process_files(input_folder: str, output_folder: str):
+    csv_file_path: str = os.path.join(output_folder, 'extracted_data.csv')
 
     with open(csv_file_path, 'w', newline='', encoding='utf-8') as csv_file:
         csv_writer = csv.writer(csv_file)
         csv_writer.writerow(['Phone Number', 'Name', 'Email', 'File Name'])
-
+        
         for file_name in os.listdir(input_folder):
-            file_path = os.path.join(input_folder, file_name)
+            file_path: str = os.path.join(input_folder, file_name)
 
             if file_name.endswith('.txt') and os.path.isfile(file_path):
                 with open(file_path, 'r', encoding='utf-8') as file:
-                    text = file.read()
+                    text: str = file.read()
                     phone_numbers, names, emails, file_name = extract_data_from_text(text, file_name)
 
                     # Writing data to a CSV file 
@@ -44,13 +45,13 @@ def process_files(input_folder, output_folder):
 
 if __name__ == "__main__":
     # Absolute address of the project 
-    script_path = os.path.abspath(__file__)
+    script_path: str = os.path.abspath(__file__)
     
     # Absolute address of the "txt_files" folder 
-    input_folder = os.path.abspath(os.path.join(script_path, '..', '..', 'txt_files'))
+    input_folder: str = os.path.abspath(os.path.join(script_path, '..', '..', 'txt_files'))
     
     # Absolute address of the "csv_files" folder 
-    output_folder = os.path.abspath(os.path.join(script_path, '..', '..', 'csv_files'))
+    output_folder: str = os.path.abspath(os.path.join(script_path, '..', '..', 'csv_files'))
     
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
